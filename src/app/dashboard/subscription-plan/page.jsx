51afd62c56fcba2a2a69264dfa4a601f29b05a87
navtej0110 <nvtej.0110@gmail.com>
2025-11-11 11:07:20 +0530

    added Subscription plan and referral Link functionality

--- src/app/dashboard/subscription-plan/page.jsx
+++ src/app/dashboard/subscription-plan/page.jsx
@@ -1,21 +1,26 @@
+// C:\Users\Admin\Desktop\React\NextJS\live\fashion-app\src\app\dashboard\subscription-plan\page.jsx
+import React from "react";
+import SubscriptionPlans from "./SubscriptionPlans";
+import { getUser } from "@/actions/authActions";
+import { redirect } from "next/navigation";
 
-import React from 'react';
-import SubscriptionPlans from './SubscriptionPlans';
-import { getUser } from '@/actions/authActions';
-
-const page =async () => {
-  const response = await getUser();
-
-  if (response.status != 200) {
-    throw new Error("Failed to fetch user profile");
+export const dynamic = 'force-dynamic';
+const page = async () => {
+  try {
+    const response = await getUser();
+    if (response.status != 200) {
+      redirect("/login");
+    }
+  } catch (error) {
+    redirect("/login");
   }
-  
+
   const user = JSON.parse(response.data);
 
   return (
     // <h3>In Progress</h3>
-    <SubscriptionPlans user={user}/>
-  )
-}
+    <SubscriptionPlans user={user} />
+  );
+};
 
-export default page
+export default page;
