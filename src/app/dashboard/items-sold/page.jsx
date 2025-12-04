f4f8289e44dd81e4e904fbdd4e120f3275ae4e49
navtej0110 <nvtej.0110@gmail.com>
2025-12-04 18:29:06 +0530

    Fixed Ui of sold Item page

--- src/app/dashboard/items-sold/page.jsx
+++ src/app/dashboard/items-sold/page.jsx
@@ -1,13 +1,13 @@
 import { getUserProductsSold } from "@/actions/productActions";
 import React from "react";
 import SoldTable from "./SoldTable";
+import { MdOutlineProductionQuantityLimits } from "react-icons/md";
 
 export const dynamic = "force-dynamic";
 
-
 export const metadata = {
-  title: 'Items Sold',
-}
+  title: "Items Sold",
+};
 
 const page = async () => {
   // const products=[];
@@ -20,7 +20,21 @@ const page = async () => {
 
   // Check if products is a valid array and not empty
   if (!Array.isArray(products) || products.length === 0) {
-    return <div>No sold products found.</div>;
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <div className="bg-white p-8 rounded-lg shadow-lg w-[50%] text-center">
+          <MdOutlineProductionQuantityLimits
+            className="mx-auto mb-4"
+            size={48}
+          />
+          <h2 className="text-2xl font-bold mb-4">No sold products found.</h2>
+          <p className="text-gray-700">
+            There are no payment transactions for sold products available at the
+            moment.
+          </p>
+        </div>
+      </div>
+    );
   }
 
   return (
